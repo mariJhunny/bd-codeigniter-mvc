fb80545081150fa8dc43614c2cadbb595dcf0528
maribelJhunny <marimu9188@gmail.com>
2019-09-12 13:13:48 -0400

    Clases

--- application/controllers/Principal.php
+++ application/controllers/Principal.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Principal extends CI_Controller {
-	public function index()
+	public function index(){
 	 
         $this->load->view('plantilla/cabecera');
         $this->load->view('plantilla/menuizquierdo');
